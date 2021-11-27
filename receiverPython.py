785b38d32ed57d630844051e35b670cfb261e5cd
yaguts1 <64816747+yaguts1@users.noreply.github.com>
2021-11-27 13:04:54 +0000

    Update receiverPython.py

--- receiverPython.py
+++ receiverPython.py
@@ -1,14 +1,21 @@
 import serial
 import csv
+import os
 
-serialPort = serial.Serial("COM7", baudrate=9600,timeout=2)
-
+serialPort = serial.Serial("COM7", baudrate=115200)
 arduinoData = serialPort.readline()
-while(True):
-    arduinoData = serialPort.readline().decode("ascii")
-    print(arduinoData)
-#add the data to the file
-    file = open("receiverDb.csv", "a") #append the data to the file
-    file.write(arduinoData) #write data with a newline
-#close out the file
-file.close()
+try:
+    os.rename('output.csv', 'ALTERAR_MEU_NOME.csv')
+except IOError:
+    print('')
+finally:
+    
+    
+    while(True):
+        arduinoData = serialPort.readline().decode("ascii")
+        print(arduinoData)
+    #add the data to the file
+        file = open("output.csv", "a") #append the data to the file
+        file.write(arduinoData) #write data with a newline
+    #close out the file
+    file.close()
